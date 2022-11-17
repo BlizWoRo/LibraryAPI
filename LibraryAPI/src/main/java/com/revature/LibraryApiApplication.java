258d53995d3df4279fed26c8b3f6b74b5176af77
BlizWoRo <saumik148@revature.net>
2022-11-17 15:16:59 -0600

    team2 front with swagger

--- LibraryAPI/src/main/java/com/revature/LibraryApiApplication.java
+++ LibraryAPI/src/main/java/com/revature/LibraryApiApplication.java
@@ -2,12 +2,43 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class LibraryApiApplication {
+public class LibraryApiApplication implements WebMvcConfigurer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
+	}
+	
+	@Configuration
+	@EnableSwagger2WebMvc
+	public class SpringFoxConfig {                                    
+	    @Bean
+	    public Docket api() { 
+	        return new Docket(DocumentationType.SWAGGER_2)  
+	          .select()                                  
+	          .apis(RequestHandlerSelectors.any())              
+	          .paths(PathSelectors.any())                          
+	          .build();                                           
+	    }
+	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
 }
